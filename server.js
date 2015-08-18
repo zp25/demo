ce750a9be9271b34643ec0ecbd893afc234edea0
zp25 <zebrap25@gmail.com>
2015-08-19 01:18:40 +0800

    重设结构

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var http = require('http');
 var fs = require('fs');
-// var exphbs  = require('express-handlebars');
+var exphbs  = require('express-handlebars');
 var favicon = require('serve-favicon');
 var multer = require('multer');
 var compression = require('compression');
@@ -10,20 +10,41 @@ var app = express();
 var port = process.env.PORT || 8080;
 var oneDay = 86400000;
 
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
+// check mode
+if (app.get('env') !== 'production') {
+  console.log('Development mode');
+}
 
-// compress
+// view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// use middleware
 app.use(compression());
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static(__dirname + "/public", { maxAge: oneDay }));
-app.use(express.static(__dirname + "/pages"));
+app.get('/:page_name', function(req, res) {
+  fs.readFile(__dirname + '/pages/' + req.params.page_name, { encoding: 'utf8' }, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      return res.end('Page Not Found');
+    }
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+    res.render('pages', { container: data });
+  });
+});
+
+app.use(express.static(__dirname + "/public", { maxAge: oneDay }));
+
+app.use(function(err, req, res, next) {
+  res.status(404).send('Page Not Found');
+});
+
 
 // file upload
 var upload = multer({
@@ -43,6 +64,7 @@ var upload = multer({
 app.post('/uploads', upload.single('fileField'), function (req, res, cb) {
   res.status(204).end();
 });
+
 
 // engine start!
 var server = http.createServer(app).listen(port, function() {
