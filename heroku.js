fdafde8cac3c9cb5c8337a81436dd64e35fe8f4a
zp25 <zebrap25@gmail.com>
2015-12-29 11:21:34 +0800

    更新gulp-imagemin

--- heroku.js
+++ heroku.js
@@ -4,7 +4,7 @@
 if (process.env.NODE_ENV === 'production') {
 
   var child_process = require('child_process');
-  var cmd = 'npm run gulp --gulpfile gulpfile.production.js';
+  var cmd = 'gulp --gulpfile gulpfile.production.js';
 
   return child_process.exec(cmd, function (err, stdout, stderr) {
     console.log('stdout: ' + stdout);
