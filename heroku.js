175e8949112fbccc8696685d14cfb20465b565ae
zp25 <zebrap25@gmail.com>
2017-03-17 16:40:09 +0800

    build: 使用yarn, 更新依赖, 更新heroku部署

--- heroku.js
+++ heroku.js
@@ -2,16 +2,16 @@
 
 // 仅在production时运行，防止本地npm install时运行
 if (process.env.NODE_ENV === 'production') {
+  const exec = require('child_process').exec;
+  const cmd = 'gulp --gulpfile gulpfile-production.babel.js';
 
-  var child_process = require('child_process');
-  var cmd = 'gulp --gulpfile gulpfile-production.babel.js';
-
-  return child_process.exec(cmd, function (err, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (err !== null) {
-      console.log('exec error: ' + err);
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.log(`exec error: ${err}`);
+      return;
     }
-  });
 
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
