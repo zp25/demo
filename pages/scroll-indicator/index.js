242c612856c077b9af10fd8a663b3fa53fccfcd5
zp25 <zebrap25@gmail.com>
2017-06-15 17:27:33 +0800

    feat: 添加scroll-indicator

---
+++ pages/scroll-indicator/index.js
@@ -0,0 +1,63 @@
+const templater = require('templater'); // eslint-disable-line import/no-extraneous-dependencies
+const loremIpsum = require('lorem-ipsum');
+
+const { header, link, style, script } = require('../public');
+
+const lorem = {
+  words: {
+    count: 2,
+    units: 'words',
+  },
+  sentences: {
+    count: 10,
+    units: 'sentences',
+  },
+};
+
+const list = (len = 5) => {
+  const arr = new Array(len);
+  arr.fill(0);
+
+  const li = arr.reduce(prev => `${prev}<li>${loremIpsum(lorem.words)}</li>`, '');
+
+  return `<ul>${li}</ul>`;
+};
+
+const section = (len = 8) => {
+  const arr = new Array(len);
+  arr.fill(0);
+
+  return arr.reduce((prev, d, index) => (
+    index === 2 ? prev + list() : `${prev}<p>${loremIpsum(lorem.sentences)}</p>`
+  ), '');
+};
+
+const fakeArticle = () => {
+  const arr = new Array(3);
+  arr.fill(0);
+
+  return arr.reduce(prev => prev + section(), '');
+};
+
+module.exports = templater `
+  <div id="app" class="container container--scroll-indicator">
+    ${header}
+
+    <main class="main flex">
+      ${link}
+
+      <section class="col col--s12 section" id="scroll-indicator">
+        ${fakeArticle}
+      </section>
+
+      <section class="col col--s12 section">
+        <p>code会覆盖indicator</p>
+      </section>
+
+      <section class="col col--s12 code">
+        ${style}
+        ${script('code')}
+      </section>
+    </main>
+  </div>
+`;
